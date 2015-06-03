c77c8090da29efe2a4960aec0fd4a386a9e95ed5
shengli1989 <shengli@shenglis-MBP.local>
2015-06-03 15:09:53 +0800

    調整大框架以及新增頁面內容縮放特效

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,26 +1,35 @@
-// var waypoints = $('.site-info').waypoint({
-//   handler: function(direction) {
-//     $('body').toggleClass('is-scaled');
-//   },
-//   offset: '100%'
-// })
+if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 
-$('.site-list').slick({
-  centerMode: true,
-  centerPadding: '11.25%',
-  slidesToShow: 1,
-  arrows: false,
-  mobileFirst: true,
-  responsive: [
-    {
-      breakpoint: 440,
-      settings: {
-        slidesToShow: 2
+  var target = $('body');
+  var translate_target = $('.page');
+  var desktop_breakpoint = 960;
+  target.addClass('scale-content');
+
+  $(window).on('scroll', function(){
+    var target_scrolltop = target.scrollTop();
+    var target_offsettop = target[0].offsetHeight - $(window).height();
+    if (target_scrolltop == target_offsettop) {
+      var viewport_width = $(window).width();
+      if (viewport_width >= desktop_breakpoint) {
+        var x = -($(window).height() * 0.8) + 'px';
+        translate_target.css('transform', 'translateY('+x+')');
+        target.addClass('is-scaled');
       }
-    },
-    {
-      breakpoint: 640,
-      settings: 'unslick'
+    } else {
+      target.removeClass('is-scaled');
+      translate_target.css('transform', 'translateY(0)');
     }
-  ]
-});
+  });
+
+  $(window).resize(function() {
+    var viewport_width = $(window).width();
+    if (viewport_width < desktop_breakpoint) {
+      target.removeClass('scale-content');
+      translate_target.css('transform', 'translateY(0)');
+    } else {
+      target.addClass('scale-content');
+    }
+  });
+
+}
+
