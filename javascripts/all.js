65332149530129d428edcd1b75ef88969d5e8099
evenwu <evendesign@gmail.com>
2015-06-12 01:39:40 +0800

    update

--- javascripts/all.js
+++ javascripts/all.js
@@ -8,29 +8,38 @@ if(Modernizr.csstransitions && !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMob
 
   $(window).on('scroll', function(){
     var scrolling = doc.scrollTop();
-    var touch_bottom = body.height() - $(window).height();
+    var touch_bottom = body.height() - $(window).height() - $('.site-info').outerHeight();
     if (scrolling >= touch_bottom) {
       var viewport_width = $(window).width();
       if (viewport_width >= desktop_breakpoint) {
-        var x = -($(window).height() * 0.8) + 'px';
-        page.css('transform', 'translateY('+x+')');
-        body.addClass('is-scaled');
+        push_page();
       }
     } else {
-      body.removeClass('is-scaled');
-      page.css('transform', 'translateY(0)');
+      reset_page();
     }
   });
 
   $(window).resize(function() {
+    var scrolling = doc.scrollTop();
     var viewport_width = $(window).width();
-    if (viewport_width < desktop_breakpoint) {
+    if (viewport_width < desktop_breakpoint ) {
       body.removeClass('scale-content');
-      page.css('transform', 'translateY(0)');
+      reset_page();
     } else {
       body.addClass('scale-content');
+      push_page();
     }
   });
 
+  function push_page(){
+    page.css('transform', 'translateY('+ (150 - $(window).height() + 'px') +')');
+    body.addClass('is-scaled');
+  }
+  function reset_page(){
+    page.css('transform', 'translateY(0)');
+    body.removeClass('is-scaled');
+  }
+
 }
+
 
