9c2bcedb86836d04ca995fc7cea2ab1a14046801
evenwu <evendesign@gmail.com>
2015-06-12 05:49:29 +0800

    修正 js

--- javascripts/all.js
+++ javascripts/all.js
@@ -9,7 +9,7 @@ if(Modernizr.csstransitions && !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMob
   $(window).on('scroll', function(){
     var scrolling = doc.scrollTop();
     var touch_bottom = body.height() - $(window).height() - $('.site-info').outerHeight();
-    if (scrolling >= touch_bottom) {
+    if ( scrolling >= touch_bottom ) {
       var viewport_width = $(window).width();
       if (viewport_width >= desktop_breakpoint) {
         push_page();
@@ -21,9 +21,13 @@ if(Modernizr.csstransitions && !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMob
 
   $(window).resize(function() {
     var scrolling = doc.scrollTop();
+    var touch_bottom = body.height() - $(window).height() - $('.site-info').outerHeight();
     var viewport_width = $(window).width();
     if (viewport_width < desktop_breakpoint ) {
       body.removeClass('scale-content');
+      reset_page();
+    } else if (viewport_width >= desktop_breakpoint && scrolling < touch_bottom) {
+      body.addClass('scale-content');
       reset_page();
     } else {
       body.addClass('scale-content');
