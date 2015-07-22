a79f61e81ebee15594bc4ec2b7aaf50656141b41
shengli1989 <shengli@evendesign.tw>
2015-07-22 18:52:14 +0800

    fixed position fixed bug

--- javascripts/all.js
+++ javascripts/all.js
@@ -16,8 +16,8 @@ if(Modernizr.csstransitions && !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMob
   }
   function reset_page(){
     page.css({
-      "-webkit-transform": 'translate(0,0)',
-      "transform": 'translate(0,0)'
+      "-webkit-transform": 'none',
+      "transform": 'none'
     });
     body.removeClass('is-scaled');
   }
